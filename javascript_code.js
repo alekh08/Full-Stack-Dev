50fd086c288aa7cbb11af692727e9f5331026962
alekh08 <137406174+alekh08@users.noreply.github.com>
2025-04-29 14:54:12 +0530

    cc

---
+++ javascript_code.js
@@ -0,0 +1,7 @@
+// write a JavaScript function that calculates the square of a number . Add comments to explain each part of code.
+ 
+// write a javascript program to declares variables of different data types(string,number,Boolean) and prints their values and types.
+
+// write a javascript program that uses arithmetic operators to calculate the sum,difference , product and quotient of two numbers.Display the results.
+
+// Write a js program that converts a string to a number and a number to a string. show the results and their types before and after the conversion
